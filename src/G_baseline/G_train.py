812aafbe0329d7ba1d01433ec09ddf712ca67c65
moonlightlane <wangzichao6@gmail.com>
2017-08-11 23:41:30 -0500

    minor update

--- src/G_baseline/G_train.py
+++ src/G_baseline/G_train.py
@@ -19,6 +19,23 @@ import time
 
 use_cuda = torch.cuda.is_available()
 
+
+import time
+import math
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
 ######################################################################
 # Training the Model
 # context = input_variable
