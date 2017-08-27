81037a5c829b625eb942e7ea47be9e21abbcb92f
moonlightlane <wangzichao6@gmail.com>
2017-08-27 16:12:53 -0500

    updated generator and GAN pretrain loading for experiment pretrain

--- src/GAN_model/GAN_model.py
+++ src/GAN_model/GAN_model.py
@@ -46,7 +46,7 @@ class GAN_model(nn.Module):
                  G_dec_input_size, G_dec_hidden_size, G_output_size, G_dec_n_layers, G_dec_num_directions,
                  D_enc_input_size, D_enc_hidden_size, D_enc_n_layers, D_num_directions,
                  D_mlp_hidden_size, D_num_attn_weights, D_mlp_output_size,
-                 use_attn, batch_size, pretrain=False
+                 use_attn, batch_size, pretrain=False, G_path=None
                  ):
 
         super(GAN_model, self).__init__()
@@ -55,8 +55,10 @@ class GAN_model(nn.Module):
             self.G = G(G_enc_input_size, G_enc_hidden_size, G_enc_n_layers, G_enc_num_directions, G_dec_input_size,
                    G_dec_hidden_size, G_output_size, G_dec_n_layers, G_dec_num_directions, batch_size)
         else:
-            
-            
+            # load the G model from G_path
+            # TODO: as tes tnow, I fix the path to pretrained model
+            self.G = torch.load('/home/jack/Documents/QA_QG/exp_results_temp/G_pretrain_exp_0827/generator_temp.pth')
+
         self.D = D(D_enc_input_size, D_enc_hidden_size, D_enc_n_layers, D_num_directions, D_mlp_hidden_size,
                    D_num_attn_weights, D_mlp_output_size, use_attn, batch_size)
 
