abac934729ad75b9bb044fb16f4cddcf2f4baa0c
moonlightlane <wangzichao6@gmail.com>
2017-09-07 11:05:47 -0500

    gan train main update

--- src/GAN_model/GAN_model.py
+++ src/GAN_model/GAN_model.py
@@ -1,7 +1,7 @@
 import sys, os
-sys.path.append(os.path.abspath(__file__ + "../") + 'G_c_a_sep')
-sys.path.append(os.path.abspath(__file__ + "../") + 'D_baseline')
-sys.path.append(os.path.abspath(__file__ + "../") + 'util')
+sys.path.append(os.path.abspath(__file__ + "/../") + 'G_c_a_sep')
+sys.path.append(os.path.abspath(__file__ + "/../") + 'D_baseline')
+sys.path.append(os.path.abspath(__file__ + "/../") + 'util')
 from data_proc import *
 print(os.path.abspath(__file__ + "../") + 'G_c_a_sep')
 from G_c_a_sep import G
